38a3a25c19c589c3b421e0d904c6a8fb81dccf41
Aviel Dardashti <aidardas@usc.edu>
2021-04-01 17:17:31 -0700

    That was way easier than I thought it would be.

--- back/src/Server.java
+++ back/src/Server.java
@@ -2,27 +2,46 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 public class Server {
 	public static void main(String[] args) throws Exception {
-		System.out.println("Hello world");
-//        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-//        server.createContext("/test", new MyHandler());
-//        server.setExecutor(null); // creates a default executor
-//        server.start();
+        HttpServer server = HttpServer.create(new InetSocketAddress(80), 0);
+        server.createContext("/", new RootHandler());
+        server.createContext("/sbout", new AboutHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start(); 
     }
-//
-//    static class MyHandler implements HttpHandler {
-//        @Override
-//        public void handle(HttpExchange t) throws IOException {
-//            String response = "This is the response";
-//            t.sendResponseHeaders(200, response.length());
-//            OutputStream os = t.getResponseBody();
-//            os.write(response.getBytes());
-//            os.close();
-//        }
-//    }
+
+    static class RootHandler implements HttpHandler {
+    	
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "hello";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+    
+    static class AboutHandler implements HttpHandler {
+
+		@Override
+		public void handle(HttpExchange t) throws IOException {
+			Path filename = Path.of("front/about.html");
+			String response = Files.readString(filename);
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+		}
+    }
+    
+    
 }
