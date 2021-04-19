4516c60523427c9fc6f36c2e7c33c5136e9ef665
Aviel Dardashti <aidardas@usc.edu>
2021-04-19 13:16:48 -0700

    Virtually completed front page/login page, and added redirect.

---
+++ back/src/redirect.java
@@ -0,0 +1,29 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/redirect")
+public class redirect extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public redirect() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append(request.getParameter("id_token"));
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append(request.getParameter("id_token"));
+	}
+
+}
