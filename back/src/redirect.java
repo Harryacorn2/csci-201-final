287d7a90dacebff2cd0d941ad77bdcebc17dac72
Aviel Dardashti <aidardas@usc.edu>
2021-04-20 10:55:42 -0700

    Got decryption working. Now we can actually start.

--- back/src/redirect.java
+++ back/src/redirect.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Base64;
 import java.util.Collections;
 
 import javax.servlet.ServletException;
@@ -10,8 +12,8 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.HttpTransport;
 
-import com.google.api.client.http.javanet.NetHttpTransport;
 
 
 @WebServlet("/redirect")
@@ -31,59 +33,17 @@ public class redirect extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		String idToken = request.getParameter("id_token");
+		String token = request.getParameter("id_token");
 		
+		String[] chunks = token.split("\\.");
+		Base64.Decoder decoder = Base64.getDecoder();
+
+		String header = new String(decoder.decode(chunks[0]));
+		String payload = new String(decoder.decode(chunks[1]));
 		
+		System.out.println(header);
+		System.out.println(payload);
 		
-		
-		
-		
-		
-		
-//		NetHttpTransport transport = new NetHttpTransport();
-//		GsonFactory jsonFactory = new GsonFactory();  // For some reason half of these libraries dont actually exist.
-//		
-//		GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(transport, jsonFactory)
-//			    // Specify the CLIENT_ID of the app that accesses the backend:
-//			    .setAudience(Collections.singletonList("633532092157-5at1c44d3f4qif55icb4nrjgokiu6vb9.apps.googleusercontent.com"))
-//			    // Or, if multiple clients access the backend:
-//			    //.setAudience(Arrays.asList(CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3))
-//			    .build();
-//
-//			// (Receive idTokenString by HTTPS POST)
-//
-//			GoogleIdToken idToken = null;
-//			try {
-//				idToken = verifier.verify(request.getParameter("id_token"));
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//			}
-//			
-//			if (idToken != null) {
-//			  Payload payload = idToken.getPayload();
-//
-//			  // Print user identifier
-//			  String userId = payload.getSubject();
-//			  System.out.println("User ID: " + userId);
-//
-//			  // Get profile information from payload
-//			  String email = payload.getEmail();
-//			  boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
-//			  String name = (String) payload.get("name");
-//			  String pictureUrl = (String) payload.get("picture");
-//			  String locale = (String) payload.get("locale");
-//			  String familyName = (String) payload.get("family_name");
-//			  String givenName = (String) payload.get("given_name");
-//			  String sub = (String) payload.get("sud");
-//
-//			  // Use or store profile information
-//			  // ...
-//			  System.out.println(name);
-//			  System.out.println(sub);
-//
-//			} else {
-//			  System.out.println("Invalid ID token.");
-//			}
 	}
 
 }
